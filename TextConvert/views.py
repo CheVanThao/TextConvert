839ae3b55ee32f53edfd7edcb830b37c1c02f198
Che Van Thao <chevanthao96@gmail.com>
2024-02-29 12:57:56 +0900

    adding staticfiles

--- TextConvert/views.py
+++ TextConvert/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django import forms
 
 def homePage(request):
     return render(request, "Home.html")
+
+class CommentForm(forms.Form):
+    name = forms.CharField()
+    url = forms.URLField()
+    comment = forms.CharField(widget=forms.Textarea)
