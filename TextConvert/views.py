f0290a3939e4325074f825d0a049a7a225ecd764
Che Van Thao <chevanthao96@gmail.com>
2024-03-06 20:07:45 +0900

    test

--- TextConvert/views.py
+++ TextConvert/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django import forms
+from .models import Post
 
 def homePage(request):
     return render(request, "Home.html")
@@ -8,3 +9,8 @@ class CommentForm(forms.Form):
     name = forms.CharField()
     url = forms.URLField()
     comment = forms.CharField(widget=forms.Textarea)
+    
+    
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'your_app/post_list.html', {'posts': posts})
